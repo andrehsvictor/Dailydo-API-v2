3158ca9878b35c132a15829899cab6959588c943
André Victor <andrehsvictor@gmail.com>
2024-02-18 08:22:50 -0300

    first commit

---
+++ src/main/java/andrehsvictor/dailydo/DailydoApplication.java
@@ -0,0 +1,13 @@
+package andrehsvictor.dailydo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailydoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailydoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrehsvictor/dailydo/DailydoApplicationTests.java
@@ -0,0 +1,13 @@
+package andrehsvictor.dailydo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailydoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
